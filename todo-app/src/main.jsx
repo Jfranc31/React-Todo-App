6e4717e28b1d0a40b24a406875c1301f191f86ad
Juan Franco <davidfranco923@gmail.com>
2025-12-11 20:45:50 -0500

    feat: set basename for Router in main.jsx to '/React-Todo-App'

--- todo-app/src/main.jsx
+++ todo-app/src/main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename='/React-Todo-App'>
         <App />
       </Router>
     </Provider>
